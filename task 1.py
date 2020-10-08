d6693d2f90dbf8e635dafd94bbe837045c7a53c2
Vladislav M <vladmalov1998@mail.ru>
2020-10-08 13:59:32 +0300

    add func for task1

--- task 1.py
+++ task 1.py
@@ -1,5 +1,10 @@
-hour_price = int(input("В час заработал"))
-hours_quantity = int(input("Выроботка"))
-bonus = int(input("Премия"))
-
-print((hour_price * hours_quantity) + bonus)
+def sal():
+	try:
+		hour_price = float(input("В час заработал"))
+		hours_quantity = int(input("Выроботка"))
+		bonus = int(input("Премия"))
+		res = hour_price * hours_quantity + bonus
+		print(f"зароботная плата сотрудника {res}")
+	except ValueError:
+		return print("write number")
+sal()
